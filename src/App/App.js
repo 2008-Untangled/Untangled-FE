0be7a4e3aa405a2e4d0b33dbfa2d68d84c440f37
Estelle Staffieri <estellelareinne@gmail.com>
2021-02-22 15:44:27 -0700

    Wrap main view in navigationContainer

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text style={styles.text}>I love turtles</Text>
       <StatusBar style='auto' />
     </View>
+    </NavigationContainer>
   );
 }
 
