bfe279dfd20f97b4c0a4c9b412b06397e43075a5
Cameron Romo <cromo1225@gmail.com>
2021-02-23 14:48:07 -0700

    Add function definitions for api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,16 @@
+export const getAllRooms = async (id) => {
+  const response = await fetch(`https://untangled-be.herokuapp.com/api/v1/users/${id}/rooms`)
+  return await response.json();
+}
+export const getRoom = async (id) => {
+  const response = await fetch('https://untangled-be.herokuapp.com/api/v1')
+  return await response.json();
+} 
+export const getUser = async (id) => {
+  const response = await fetch(`https://untangled-be.herokuapp.com/api/v1/users/${id}`)
+  return await response.json();
+}
+export const getMemories = async (id) => {
+  const response = await fetch(`https://untangled-be.herokuapp.com/api/v1/rooms/${id}/memories`)
+  return await response.json();
+}
