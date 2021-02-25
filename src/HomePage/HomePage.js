30ee2663a68f803e1a9a20e388075d56eaa54750
Joe Lopez <lopez.4.joseph@gmail.com>
2021-02-24 17:38:45 -0700

    Handle API call to fetch allRooms

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { Button } from "react-native-paper";
-import { getAllRooms } from '../../apiCalls';
-import Overview from '../Overview/Overview'
+import { getAllRooms } from "../apiCalls";
+import Overview from "../Overview/Overview";
 
-const HomePage = () => {
+export const HomePage = ({ navigation }) => {
+  const [allRooms, setAllRooms] = useState([]);
+
+  useEffect(() => {
+    console.log("This fucking works");
+  }, [allRooms]);
+
+  const resolveGetAllRooms = async () => {
+    await getAllRooms(1)
+      .then((data) => setAllRooms(data.data))
+      .catch((error) => console.error(error));
+  };
+
   const handlePress = () => {
+    // debugger;
+    console.log(navigation);
+    console.log("hello");
+    resolveGetAllRooms();
+    // console.log(allRooms);
+    // <Overview rooms={allRooms} />;
+  };
 
-  }
   return (
-    <View 
-      onPress={}
-    >
-      <Image 
-        source={require('')}
-      />
-      <Overview 
-        allRooms={}
-      />
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          // <Overview rooms={allRooms} />;
+          handlePress();
+          navigation.navigate("Overview", { name: "Overview" });
+        }}>
+        <Image source={require("../../assets/backdrops/homefront.png")} />
+      </TouchableOpacity>
     </View>
-  ) 
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
-}
+  container: {},
+});
