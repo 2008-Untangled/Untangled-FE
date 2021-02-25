64bf9d91290bc6aed4eb3ca7b18eef3005976b8f
Joe Lopez <lopez.4.joseph@gmail.com>
2021-02-24 17:38:13 -0700

    Fix image path

--- src/Room/Room.js
+++ src/Room/Room.js
@@ -3,22 +3,23 @@ import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 
 export default Room = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Living Room</Text>
-            <Image source={require("/assets/roomImages/livingroom.png")} style={{width: 100% , height: 100%}}></Image>
-            <StatusBar style='auto' />
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <Text>Living Room</Text>
+      <Image
+        source={require("../../assets/backdrops/livingroom.png")}
+        style={{ width: 100, height: 100 }}></Image>
+      <StatusBar style='auto' />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        backgroundColor: "#fff",
-        fontWeight: "bold",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    display: "flex",
+    backgroundColor: "#fff",
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
