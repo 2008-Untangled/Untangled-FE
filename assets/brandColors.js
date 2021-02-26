f99f07e6d0b16270424b22e3c1de9b4b2a26dc0b
Estelle Staffieri <estellelareinne@gmail.com>
2021-02-26 13:54:27 -0700

    Add brand colors to assets folder

---
+++ assets/brandColors.js
@@ -0,0 +1,20 @@
+//==== Brand Colors 2/26/2021 ==== //
+// ==== Primary Brand Colors ==== //
+#8fa08e
+#515c2e
+#e1a555
+#733425
+#81382a
+#d99380
+#8d693e
+#FFFFFF
+#000000
+// ==== Secondary Brand Colors ==== //
+#505b2e
+#768243
+#fde4db
+#c55837
+// ==== Accent Brand Colors ==== //
+#e1a555
+#d25206
+#272626
